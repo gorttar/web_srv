56ef8950fc558e1de8f3671f6c59ffcd055ee1fe
Andrey Antipov <andrey.antipov@maxifier.com>
2018-03-20 01:56:59 +0400

    implement SessionManager

--- src/main/java/helpers/hibernate/SessionManager.kt
+++ src/main/java/helpers/hibernate/SessionManager.kt
@@ -19,7 +19,20 @@ class SessionManager(private val entityManagerFactory: EntityManagerFactory) {
      * @param transactionBody to be evaluated under [EntityTransaction]
      * @return transaction body evaluation result
      */
-    fun <T> withTransaction(transactionBody: (EntityManager) -> T): T = TODO()
+    fun <T> withTransaction(transactionBody: (EntityManager) -> T): T =
+            withSession {
+                val tx = it.transaction
+                try {
+                    tx.begin()
+                    val result = transactionBody(it)
+                    tx.commit()
+                    result
+                } catch (e: Throwable) {
+                    tx.rollback()
+                    throw e
+                }
+            }
+
 
     /**
      * evaluates function represented by body and returns it's result
@@ -27,5 +40,12 @@ class SessionManager(private val entityManagerFactory: EntityManagerFactory) {
      * @param body to be evaluated with [EntityManager] as argument
      * @return body evaluation result
      */
-    fun <T> withSession(body: (EntityManager) -> T): T = TODO()
+    fun <T> withSession(body: (EntityManager) -> T): T {
+        val em = entityManagerFactory.createEntityManager()
+        return try {
+            body(em)
+        } finally {
+            em.close()
+        }
+    }
 }
